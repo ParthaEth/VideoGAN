5c37cc3f2eb7daf4fd1d4662f5fba2947623c63b
Partha Ghosh <pghosh@brown.is.localnet>
2023-04-27 15:12:33 +0200

    Reducing discriminator video time resolution to 128 frames tick 60 fid 191

--- eg3d/training/dual_discriminator.py
+++ eg3d/training/dual_discriminator.py
@@ -205,12 +205,12 @@ class DualPeepDicriminator(torch.nn.Module):
         video_color_channels = 256  # video frame chnannels
         video_resolution = img_resolution//8
         sr_upsample_factor = 1
-        self.vid_discrim = VideoDiscriminator(seq_length=256, max_edge=32, channels=3)
+        self.vid_discrim = VideoDiscriminator(seq_length=128, max_edge=32, channels=3)
 
     def forward(self, img, c, update_emas=False, **block_kwargs):
         img_pair_logits = self.image_pair_discrim(img, c * 0, update_emas=update_emas, **block_kwargs)
         b_size, c_ch, h, w, t_steps = img['peep_vid'].shape
-        vid_as_b_c_d_h_w = img['peep_vid'].permute(0, 1, 4, 2, 3)
+        vid_as_b_c_d_h_w = img['peep_vid'].permute(0, 1, 4, 2, 3)[:, :, ::2, :, :]
         vid_logits = self.vid_discrim(vid_as_b_c_d_h_w)
 
         return img_pair_logits + vid_logits
