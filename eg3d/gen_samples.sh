965705fb141c1cb926c85fed9aaab49f9489e7ec
Partha Ghosh <pghosh@brown.is.localnet>
2023-09-03 18:10:02 +0200

    Code to generate lots of samples using the cluster

---
+++ eg3d/gen_samples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if an argument is provided for run_id
+if [ $# -eq 0 ]; then
+    echo "Please provide a value for run_id."
+    exit 1
+fi
+
+run_id="$1"
+vids_p_run=200
+offset=179
+
+# Calculate start_id and end_id
+start_id=$((run_id * vids_p_run + offset))
+end_id=$((start_id + vids_p_run))
+
+CC=gcc-7
+
+/home/pghosh/miniconda3/envs/VideoGan80GB_STG_T/bin/python gen_samples.py \
+--outdir="/is/cluster/fast/pghosh/ouputs/video_gan_runs/ten_motions/\
+00086-ffhq-ffhq_X_10_good_motions_10_motions-gpus8-batch32-gamma1/talking_faces_vid" \
+--network="/is/cluster/fast/pghosh/ouputs/video_gan_runs/ten_motions/\
+00086-ffhq-ffhq_X_10_good_motions_10_motions-gpus8-batch32-gamma1/network-snapshot-003680.pkl" \
+--seeds $start_id-$end_id --img_type sr_image --trunc=0.7 --show_flow False
