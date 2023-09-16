4e038a5e570a2b211e1729fd9673af441e404e8f
Partha Ghosh <pghosh@brown.is.localnet>
2023-09-16 14:44:11 +0200

    Truncation of 0.9 seems to get best FVD for Sky timelapse

--- eg3d/gen_samples.sh
+++ eg3d/gen_samples.sh
@@ -7,8 +7,8 @@ if [ $# -eq 0 ]; then
 fi
 
 run_id="$1"
-vids_p_run=20
-offset=2001
+vids_p_run=21
+offset=0
 
 # Calculate start_id and end_id
 start_id=$((run_id * vids_p_run + offset))
@@ -16,16 +16,21 @@ end_id=$((start_id + vids_p_run))
 
 CC=gcc-7
 
-#/home/pghosh/miniconda3/envs/VideoGan80GB_STG_T/bin/python gen_samples.py \
-#--outdir="/is/cluster/fast/pghosh/ouputs/video_gan_runs/ten_motions/\
-#00086-ffhq-ffhq_X_10_good_motions_10_motions-gpus8-batch32-gamma1/talking_faces_vid" \
-#--network="/is/cluster/fast/pghosh/ouputs/video_gan_runs/ten_motions/\
-#00086-ffhq-ffhq_X_10_good_motions_10_motions-gpus8-batch32-gamma1/network-snapshot-003680.pkl" \
-#--seeds $start_id-$end_id --img_type sr_image --trunc=0.7 --show_flow False
+#echo "run_id = 000"
+#out_dir="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/00000-ffhq-video_clips-gpus8-batch128-gamma1/\
+#videos"
+#network="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/00000-ffhq-video_clips-gpus8-batch128-gamma1/\
+#network-snapshot-001228.pkl"
+#
+#/home/pghosh/miniconda3/envs/VideoGan80GB_STG_T_2/bin/python gen_samples.py \
+#--outdir=$out_dir --network=$network --seeds $start_id-$end_id --img_type sr_image --trunc=0.7 --show_flow False
+
+
+echo "run_id = 007"
+out_dir="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/00007-ffhq-video_clips-gpus8-batch128-gamma1/\
+videos_net_3440_fid_lowest_trunk0.9"
+network="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/00007-ffhq-video_clips-gpus8-batch128-gamma1/\
+network-snapshot-003440.pkl"
 
 /home/pghosh/miniconda3/envs/VideoGan80GB_STG_T_2/bin/python gen_samples.py \
---outdir="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/\
-00004-ffhq-video_clips-gpus8-batch128-gamma1/videos" \
---network="/is/cluster/fast/pghosh/ouputs/video_gan_runs/sky_timelapse/00004-ffhq-video_clips-gpus8-batch128-gamma1/\
-network-snapshot-009420.pkl" \
---seeds $start_id-$end_id --img_type sr_image --trunc=0.7 --show_flow False
+--outdir=$out_dir --network=$network --seeds $start_id-$end_id --img_type sr_image --trunc=0.9 --show_flow False
